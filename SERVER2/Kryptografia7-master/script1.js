cd3bf659e2aa6561105bd10353b48757d380ea7b
BlackEtude <229726@student.pwr.edu.pl>
2017-11-28 22:56:05 +0100

    server2 update

--- SERVER2/Kryptografia7-master/script1.js
+++
@@ -1,9 +0,0 @@
-function storeData(){
-	if (typeof(Storage) !== "undefined") {
-	localStorage.setItem("r_number", document.forms[0].elements["recipient"].value);
-	localStorage.setItem("t_value", document.forms[0].elements["value"].value);
-	} else {
-    console.log("No storage support!");
-	}
-}
-	
