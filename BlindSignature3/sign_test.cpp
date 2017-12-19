41ff2ac5cbc7e979a254585e4fad37689e852a11
BlackEtude <229726@student.pwr.edu.pl>
2017-12-19 16:43:08 +0100

    correct memory deallocation

--- BlindSignature3/sign_test.cpp
+++ BlindSignature3/sign_test.cpp
@@ -2,18 +2,25 @@
 // Created by pandemic on 17.12.17.
 //
 
-
-#include <random>
 #include "sign_test.h"
+#define ITERS 1000000
 
-sign_test::sign_test() {
+sign_test::sign_test(char *priv_path, char *pub_path) {
     ctx = BN_CTX_new();
     N = BN_new();
     d = BN_new();
+    e = BN_new();
     r = BN_new();
+
+    // Load proper keys (N, d), (N, e)
+    read_key_from_file(priv_path, N, d);
+    read_key_from_file(pub_path, N, e);
+
+    // Run attack!
+    test();
 }
 
-void sign_test::read_key_from_file(char *path) {
+void sign_test::read_key_from_file(char *path, BIGNUM *x, BIGNUM *y) {
     std::string item_name;
     std::ifstream nameFileout;
     nameFileout.open(path);
@@ -27,76 +34,9 @@ void sign_test::read_key_from_file(char *path) {
     }
 
     const char *c = temp[0].c_str();
-    BN_hex2bn(&N, c);
+    BN_hex2bn(&x, c);
     c = temp[1].c_str();
-    BN_hex2bn(&d, c);
-
-    int len = BN_num_bytes(d);
-    unsigned char* buf = (unsigned char*)malloc(len);
-    len = BN_bn2bin(d, buf);
-
-    std::cout << "Len: " << len << std::endl;
-}
-
-void sign_test::load_publickey_from_file(char *path) {
-    std::cout << "Loading key from: " << path << std::endl << std::endl;
-    std::string item_name;
-    std::ifstream nameFileout;
-    nameFileout.open(path);
-    std::string line;
-
-    auto *temp = new std::string[2];
-
-    int i = 0;
-    while(std::getline(nameFileout, line)) {
-        temp[i] = line;
-        i++;
-    }
-
-    const char *c = temp[0].c_str();
-    BN_hex2bn(&N, c);
-    c = temp[1].c_str();
-    BN_hex2bn(&e, c);
-
-    std::cout << "N: " << BN_bn2dec(N) << std::endl << std::endl;
-    std::cout << "e: " << BN_bn2dec(e) << std::endl << std::endl;
-}
-
-double sign_test::sign_msg(BIGNUM *msg_to_sign) {
-    // s'= (m')^d (mod N)
-
-    double startsecs, endsecs, seconds;
-    uint64_t cycles;
-
-    startsecs = cpu_seconds();
-    cycles = local_benchmark(msg_to_sign, d, N);
-    endsecs = cpu_seconds();
-    seconds = endsecs - startsecs;
-
-//    printf("\t%15.0f CPU cycles\n", (double)cycles);
-//    printf("\t%15.0f CPU cycles per second\n", (double)cycles / seconds);
-//    printf("\t( %13.3f GHz)\n", (double)cycles / seconds / 1000000000.0);
-//    printf("\t%15.9f seconds\n", seconds);
-//    printf("-----------------------------------------------------------\n\n");
-
-    return (double)cycles;
-}
-
-double sign_test::message_exp(BIGNUM *msg, BIGNUM *expo, BIGNUM *modulus) {
-    double startsecs, endsecs, seconds;
-    uint64_t cycles;
-
-    startsecs = cpu_seconds();
-    cycles =  local_benchmark(msg, expo, modulus);
-    endsecs = cpu_seconds();
-    seconds = endsecs - startsecs;
-
-//    printf("\t%15.0f CPU cycles\n", (double)cycles);
-//    printf("\t%15.0f CPU cycles per second\n", (double)cycles / seconds);
-//    printf("\t( %13.3f GHz)\n", (double)cycles / seconds / 1000000000.0);
-//    printf("\t%15.9f seconds\n", seconds);
-
-    return (double)cycles;
+    BN_hex2bn(&y, c);
 }
 
 void sign_test::test() {
@@ -105,68 +45,123 @@ void sign_test::test() {
     BIGNUM *o = BN_new();
     BIGNUM *power_2 = BN_new();
     BIGNUM *two = BN_new();
+    std::string final_key = "1";
+    
+    std::vector<double> zeros;
+    std::vector<double> ones;
 
-    const int iters = 500000;
-
-    // ....1
+    // Start: 1
     BN_set_word(private_key, 1);
     // 2^1 (second bit)
     BN_set_word(power_2, 2);
     // to calculate next powers of 2
     BN_set_word(two, 2);
 
-    for(int i = 2; i < 21; i++){
-        // New possible exponents: 1...1, 1...0
+    double server_time, local_time, diff;
+    double var0, var1;
+
+    for(int i = 2; i < 18; i++) {
+        // New possible exponents: 0...1, 1...1
         BN_copy(z, private_key);
         BN_add(o, private_key, power_2);
-        std::cout << "Zero: " << BN_bn2dec(z) << ", one: " << BN_bn2dec(o) << std::endl;
+        std::cout << "Bit no "<< i << std::endl;
 
-        double sum = 0;
-        std::cout << "Bit nr "<< i << std::endl;
-
-        std::vector<double> zeros;
-        std::vector<double> ones;
-
-        for(int j = 0; j < iters; j++) {
-
+        for(int j = 0; j < ITERS; j++) {
             // Prepare new message
-            BIGNUM *rand_msg = calculate_msg();
+            BIGNUM *rand_msg = generate_msg();
 
-            // For 1...0
-            //Local expo
-            double server_time = sign_msg(rand_msg);
-            double local_time0 = message_exp(rand_msg, z, N);
-            double diff0 = server_time - local_time0;
+            server_time = sign_msg(rand_msg, d, N);
+
+            // For 0...1
+            local_time = sign_msg(rand_msg, z, N);
+            diff = server_time - local_time;
+            zeros.push_back(abs(diff));
 
             // For 1...1
-            //Local expo
-            double local_time1 = message_exp(rand_msg, o, N);
-            double diff1 = server_time - local_time1;
+            local_time = sign_msg(rand_msg, o, N);
+            diff = server_time - local_time;
+            ones.push_back(abs(diff));
 
-            zeros.push_back(abs(diff0));
-            ones.push_back(abs(diff1));
+            BN_free(rand_msg);
         }
 
+        var0 = array_var(zeros);
+        var1 = array_var(ones);
 
-        double var0 = array_average(zeros);
-        double var1 = array_average(ones);
+        printf("'Zero' var: %e\n", var0);
+        printf("'One' var: %e\n", var1);
 
-        printf("Zero var: %e\n", var0);
-        printf("Ones var: %e\n", var1);
-
-        // Analize result and choose new exponent
-        //If more 'ones' than 'zeros'
+        // Analize results and choose new exponent's bit
         if(var1 < var0) {
             BN_add(private_key, private_key, power_2);
             std::cout << "VAR1 < VAR0" << std::endl;
+            final_key = '1' + final_key; 
         }
-        else
+        else {
             std::cout << "VAR0 < VAR1" << std::endl;
+            final_key = '0' + final_key; 
+        }
 
-        std::cout << "New key: " << BN_bn2dec(private_key) << std::endl;
+        BN_CTX_start(ctx);
         BN_mul(power_2, power_2, two, ctx);
-        std::cout << "----------------------------------------" << std::endl;
+        BN_CTX_end(ctx);
+        std::cout << "Key: " << final_key << std::endl << std::endl << std::endl;
+
+        // Cleanup
+        ones.clear();
+        ones.shrink_to_fit();
+        zeros.clear();
+        zeros.shrink_to_fit();
     }
+
+    // Cleanup
+    BN_free(private_key);
+    BN_free(z);
+    BN_free(o);
+    BN_free(power_2);
+    BN_free(two);
+}
+
+BIGNUM* sign_test::generate_msg() {
+    // m' = hash(m) * r^e (mod N)
+    int length = rand() % 10 + 3;
+
+    // Generate new msg
+    static const std::string::value_type allowed_chars[] {"123456789BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz"};
+    static thread_local std::default_random_engine randomEngine(std::random_device{}());
+    static thread_local std::uniform_int_distribution<int> randomDistribution(0, sizeof(allowed_chars) - 1);
+    std::string id(length, '\0');
+    for (std::string::value_type& c : id)
+        c = allowed_chars[randomDistribution(randomEngine)];
+
+    const char *message = id.c_str();
+    std::string hashed_msg = sha256(message);
+    const char *hashed_msg_char = hashed_msg.c_str();
+    BIGNUM *m = BN_new();
+    BN_hex2bn(&m, hashed_msg_char);
+    const char* hashed;
+    hashed = BN_bn2dec(m);
+
+    BIGNUM *one = BN_new();
+    BIGNUM *gcd = BN_new();
+    BN_one(one);
+    BN_CTX_start(ctx);
+
+    do {
+        BN_rand_range(r, N);
+        BN_gcd(gcd,r, N, ctx);
+    }
+    while(BN_cmp(gcd, one) != 0);
+
+    BIGNUM *x = BN_new();
+    BN_mod_exp(x, r, e, N, ctx);
+    BN_mod_mul(x, m, x, N, ctx);
+
+    BN_free(one);
+    BN_free(gcd);
+    BN_free(m);
+    BN_CTX_end(ctx);
+    return x;
 }
 
 std::string sign_test::sha256(const std::string str) {
@@ -182,87 +177,28 @@ std::string sign_test::sha256(const std::string str) {
     return ss.str();
 }
 
-BIGNUM* sign_test::calculate_msg() {
-    // m' = hash(m) * r^e (mod N)
-    int length = rand() % 10 + 3;
+double sign_test::sign_msg(BIGNUM *msg_to_sign, BIGNUM *expo, BIGNUM *modulus) {
+    // s'= (m')^d (mod N)
 
-    //Generate new msg
-    static const std::string::value_type allowed_chars[] {"123456789BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz"};
-    static thread_local std::default_random_engine randomEngine(std::random_device{}());
-    static thread_local std::uniform_int_distribution<int> randomDistribution(0, sizeof(allowed_chars) - 1);
-    std::string id(length, '\0');
-    for (std::string::value_type& c : id) {
-        c = allowed_chars[randomDistribution(randomEngine)];
-    }
+    double startsecs, endsecs, seconds;
+    uint64_t cycles;
 
-    const char *message = id.c_str();
+    startsecs = cpu_seconds();
+    cycles = benchmark(msg_to_sign, expo, modulus);
+    endsecs = cpu_seconds();
+    seconds = endsecs - startsecs;
 
-    std::string hashed_msg = sha256(message);
-    const char *hashed_msg_char = hashed_msg.c_str();
-    BIGNUM *m = BN_new();
-    BN_hex2bn(&m, hashed_msg_char);
-    const char* hashed;
-    hashed = BN_bn2dec(m);
+//    printf("\t%15.0f CPU cycles\n", (double)cycles);
+//    printf("\t%15.0f CPU cycles per second\n", (double)cycles / seconds);
+//    printf("\t( %13.3f GHz)\n", (double)cycles / seconds / 1000000000.0);
+//    printf("\t%15.9f seconds\n", seconds);
+//    printf("-----------------------------------------------------------\n\n");
 
-    BIGNUM *one = BN_new();
-    BIGNUM *gcd = BN_new();
-    BN_one(one);
-
-    do {
-        BN_rand_range(r, N);
-        BN_gcd(gcd,r, N, ctx);
-    }
-    while(BN_cmp(gcd, one) != 0);
-
-    BIGNUM *x = BN_new();
-    BN_mod_exp(x, r, e, N, ctx);
-    BN_mod_mul(x, m, x, N, ctx);
-
-    BN_free(one);
-    BN_free(gcd);
-    BN_free(m);
-    return x;
+    return (double)cycles;
 }
 
-double sign_test::array_average(std::vector<double> array) {
-    unsigned long length = array.size();
-    double avg = 0;
-    for(unsigned long i = 0; i < array.size(); i++) {
-        avg += array.at(i);
-    }
-
-    avg = avg/length;
-
-    double var = 0;
-    for(unsigned long i = 0; i < array.size(); i++)
-        var += (array.at(i) - avg) * (array.at(i) - avg);
-
-    var /= (length);
-    return var;
-
-}
-
-BIGNUM* sign_test::modular_exponentation(BIGNUM *x, BIGNUM *y, BIGNUM *m) {
-    BIGNUM *result = BN_new();
-    BN_set_word(result, 1);
-
-//    x = x % m;
-    BIGNUM *zero = BN_new();
-
-    while(BN_cmp(y, zero) == 1) {                   // y > 0
-        //y is odd -> mul x with result
-        if(BN_is_odd(y) == 1)
-            BN_mod_mul(result, result, x, m, ctx);  // result = (result*x) % m;
-        //y must be even now => div by 2
-        BN_rshift1(y, y);                           // y = y >> 1;
-        BN_mod_mul(x, x, x, m, ctx);                // x = (x*x) % m;
-    }
-    return result;
-}
-
-uint64_t sign_test::local_benchmark(BIGNUM *msg, BIGNUM *expo, BIGNUM *modulus) {
+uint64_t sign_test::benchmark(BIGNUM *msg, BIGNUM *expo, BIGNUM *modulus) {
     uint64_t started, finished;
-    BIGNUM *result = BN_new();
 
     started = rdtsc();
     modular_exponentation(msg, expo, modulus);
@@ -271,19 +207,62 @@ uint64_t sign_test::local_benchmark(BIGNUM *msg, BIGNUM *expo, BIGNUM *modulus) 
     return finished - started;
 }
 
+uint64_t sign_test::rdtsc() {
+    uint32_t lo, hi;
+    __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+    return ( ((uint64_t)hi) << (uint32_t)32 )
+           | ( ((uint64_t)lo) );
+}
+
+void sign_test::modular_exponentation(BIGNUM *x, BIGNUM *y, BIGNUM *m) {
+    BIGNUM *result = BN_new();
+    BIGNUM *zero = BN_new();                        
+    BN_set_word(result, 1);
+    BN_set_word(zero, 0);
+
+
+    //    x = x % m;
+    while(BN_cmp(y, zero) == 1) {                   // y > 0
+        //y is odd -> mul x with result
+        if(BN_is_odd(y) == 1) {
+            BN_CTX_start(ctx);
+            BN_mod_mul(result, result, x, m, ctx);  // result = (result*x) % m;
+            BN_CTX_end(ctx);
+        }
+        //y must be even now => div by 2
+        BN_rshift1(y, y);                           // y = y >> 1;
+        BN_CTX_start(ctx);
+        BN_mod_mul(x, x, x, m, ctx);                // x = (x*x) % m;
+        BN_CTX_end(ctx);
+    }
+    BN_free(zero);
+    BN_free(result);
+}
+
+double sign_test::array_var(std::vector<double> array) {
+    unsigned long length = array.size();
+    double avg = 0;
+
+    for (double i : array)
+        avg += i;
+
+    avg = avg/length;
+
+    double var = 0;
+    for (double i : array)
+        var += (i - avg) * (i - avg);
+
+    var /= (length);
+    return var;
+}
+
+// Perhaps useless....
 double sign_test::cpu_seconds() {
     struct timespec t;
     if (!clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t))
         return (double)t.tv_sec + (double)t.tv_nsec / 1000000000.0;
     else
         return (double)clock() / (double)CLOCKS_PER_SEC;
-}
-
-uint64_t sign_test::rdtsc() {
-    uint32_t lo, hi;
-    __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
-    return ( ((uint64_t)hi) << (uint32_t)32 )
-           | ( ((uint64_t)lo) );
 }
 
 int main(int argc, char* argv[]) {
@@ -294,14 +273,7 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    auto *test = new sign_test();
-    // Load proper private key (N, d)
-    test->read_key_from_file(argv[1]);
-
-    // Load proper public key (N, e)
-    test->load_publickey_from_file(argv[2]);
-
-    test->test();
+    auto *test = new sign_test(argv[1], argv[2]);
 
     return 0;
 }
--- BlindSignature3/sign_test.h
+++ BlindSignature3/sign_test.h
@@ -13,9 +13,6 @@
 #include <cstdio>
 #include <cstdlib>
 #include <chrono>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <netinet/in.h>
 #include <openssl/crypto.h>
@@ -26,6 +23,7 @@
 #include <chrono>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <random>
 
 #define BUFFER_SIZE 4096
 #define SIGNATURER_SIGN_TEST_H
@@ -35,20 +33,19 @@ class sign_test {
 public:
     BIGNUM *N, *d, *e, *r;
     BN_CTX *ctx;
-    sign_test();
-    void read_key_from_file(char *path);
-    void load_publickey_from_file(char *path);
-    double sign_msg(BIGNUM *msg_to_sign);
+    
+    sign_test(char *priv_path, char *pub_path);
+    void read_key_from_file(char *path, BIGNUM *x, BIGNUM *y);
+    double sign_msg(BIGNUM *msg_to_sign, BIGNUM *expo, BIGNUM *modulus);
 
     void test();
-    BIGNUM *calculate_msg();
+    BIGNUM *generate_msg();
     std::string sha256(std::string str);
-    double message_exp(BIGNUM *msg, BIGNUM *expo, BIGNUM *modulus);
     double cpu_seconds();
     uint64_t rdtsc();
-    uint64_t local_benchmark(BIGNUM *msg, BIGNUM *expo, BIGNUM *modulus);
-    BIGNUM* modular_exponentation(BIGNUM *x, BIGNUM *y, BIGNUM *m);
-    double array_average(std::vector<double> array);
+    uint64_t benchmark(BIGNUM *msg, BIGNUM *expo, BIGNUM *modulus);
+    void modular_exponentation(BIGNUM *x, BIGNUM *y, BIGNUM *m);
+    double array_var(std::vector<double> array);
 };
 
 
