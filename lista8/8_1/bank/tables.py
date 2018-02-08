ad61e7fc940a1709f7cb95a13d292fb2217ed4f4
BlackEtude <229726@student.pwr.edu.pl>
2018-02-08 21:43:24 +0100

    cleanup

--- lista8/8_1/bank/tables.py
+++
@@ -1,9 +0,0 @@
-from django_tables2 import tables
-
-from bank.models import Transfer
-
-
-class HistoryTable(tables.Table):
-    class Meta:
-        model = Transfer
-        template = 'django_tables2/bootstrap.html'
